b9432a5b91d65e64be36b52ee0d354ca3371b76d
codermohan <48611321+codermohan@users.noreply.github.com>
2025-03-14 17:31:50 -0500

    Update test_day4.py

--- test_day4.py
+++ test_day4.py
@@ -1,10 +1,10 @@
 import unittest
-from day4 import add_numbers, power
+from day4 import add_num, power
 
 class TestFunctions(unittest.TestCase):
-    def test_add_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 5)
-        self.assertEqual(add_numbers(-1, 1), 0)
+    def test_add_num(self):
+        self.assertEqual(add_num(2, 3), 5)
+        self.assertEqual(add_num(-1, 1), 0)
 
     def test_power(self):
         self.assertEqual(power(2), 4)  # Default exponent (2^2)
