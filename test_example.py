93b0afbd925a207a38a5a158abdb3fc353e30c88
codermohan <48611321+codermohan@users.noreply.github.com>
2025-03-14 17:22:23 -0500

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -4,7 +4,7 @@ from day4 import greet_user,add_num,power  # Import the function from your Pytho
 class TestYourFunction(unittest.TestCase):
     
     def test_function(self):
-        result = greet_user()  # Call the function you're testing
+        result = greet_user("Mohan")  # Call the function you're testing
         self.assertEqual(result, expected_result)  # Replace expected_result with the correct value
 
 if __name__ == '__main__':
