5230568eb5470c75d27cfe984bf4b103b30b1e90
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-03-15 20:59:08 -0500

    Adding second loop file

---
+++ loop2.py
@@ -0,0 +1,21 @@
+#using for loop print numbers 1 to 5
+
+for i in range (1,6):
+    print ("For loop:",i)
+
+#print numbers 1 to 6 using while 
+count = 1
+
+while count<=5:
+    print("While Loop:",count)
+    count +=1
+
+
+#trying do while loop
+
+#count =1
+#do print(count)
+
+#while count<=5:
+   # print("Do while:",count)
+    #count+=1
