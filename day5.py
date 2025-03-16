8573227fc4b6b95bb3dd1b01928d72c3b0a52b7f
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-03-15 20:37:08 -0500

    Adding my new python file for day5

---
+++ day5.py
@@ -0,0 +1,20 @@
+def add(num1,num2):
+    return num1 + num2
+
+def mul(num1,num2):
+    return num1 * num2
+
+def greet(name):
+    return f'Hello {name}, Welcome to Python Series!'
+
+
+#Calling functions
+
+addition = add(7,9)
+multiply = mul(3,4)
+greeting= greet("Mohan")
+
+
+print("Addition is :",addition)
+print("Multiplication is :",multiply)
+print(greeting)
